685662b55533ed6d9f03b6efc5f8c32c2254e64c
shivamsingh050 <91783227+shivamsingh050@users.noreply.github.com>
2021-10-02 16:12:07 +0530

    Create subdomainfinder.py

---
+++ subdomainfinder.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Turbolist3r
+# By Carl Pearson - github.com/fleetcaptain
+# Based on Sublist3r code created by Ahmed Aboul-Ela - twitter.com/aboul3la
+#
+
+
+# modules in standard library
+import re
+import sys
+import os
+import argparse
+import time
+import hashlib
+import random
+import multiprocessing
+import threading
+import socket
+import time
+from collections import Counter
+
+# external modules
+try:
+    from subbrute import subbrute
+except:
+    pass
+    #print("Failed to import subbrute, you will not be able to bruteforce")
+import requests
+
+# import dnslib, which provides better features compared to dns.resolver for finding subdomains
+# for example, a return status of NXDOMAIN causes an exception with dns.resolver, but dnslib allows
+# us to easily capture the reply, which could indicate the precsence of subdomain takeover
+import dnslib
+
+# Python 2.x and 3.x compatiablity
+if sys.version > '3':
+    import urllib.parse as urlparse
+    import urllib.parse as urllib
+else:
+    import urlparse
+    import urllib
+
+# In case you cannot install some of the required development packages
+# there's also an option to disable the SSL warning:
+try:
+    import requests.packages.urllib3
+
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
+
+# Check if we are running this on windows platform
+is_windows = sys.platform.startswith('win')
+
+global debug
+global resolvers # set resolvers as global so we can use it in multiple places
+global RESOLVER_COUNT # number of resolvers we are using
+
+# Console Colors
+if is_windows:
+    # Windows deserve coloring too :D
+    G = '\033[92m'  # green
+    Y = '\033[93m'  # yellow
+    B = '\033[94m'  # blue
+    R = '\033[91m'  # red
+    W = '\033[0m'  # white
+    try:
+        import win_unicode_console, colorama
+
+        win_unicode_console.enable()
+        colorama.init()
+        # Now the unicode will work ^_^
+    except:
+        print("[!] Error: Coloring libraries not installed ,no coloring will be used [Check the readme]")
+        G = Y = B = R = W = G = Y = B = R = W = ''
+
+
+else:
+    G = '\033[92m'  # green
+    Y = '\033[93m'  # yellow
+    B = '\033[94m'  # blue
+    R = '\033[91m'  # red
+    W = '\033[0m'  # white
+
+# array of characters that can appear in an IP address
+resolver_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'] 
+
+
+
+def banner():
+    print("""%s
+           _____            _           _ _     _   _____
+          |_   _|_   _ _ __| |__  ____ | (_)___| |_|___ / _ __
+            | | | | | | `__|  _ \/    \| | / __| __| |_ \| '__|
+            | | | \_| | |  | |_) | () || | \__ \ |_ ___) | |
+            |_|  \____|_|  |_.__/\____/|_|_|___/\__|____/|_|%s%s
+          # Based on Sublist3r by Ahmed Aboul-Ela - @aboul3la
+          # Forked by Carl Pearson - github.com/fleetcaptain
+    """ % (R, W, Y))
+
+
+
+# given a string, parse and check if it is an IP address
+def is_ip(s):
+    a = s.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+
+def parser_error(errmsg):
+    banner()
+    print("Usage: python " + sys.argv[0] + " [Options] use -h for help")
+    print(R + "Error: " + errmsg + W)
+    sys.exit()
+
+
+def parse_args():
+    # parse the arguments
+    parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -d google.com")
+    parser.error = parser_error
+    parser._optionals.title = "OPTIONS"
+    parser.add_argument('-d', '--domain', help="Domain name to enumerate it's subdomains", required=True)
+    parser.add_argument('-b', '--bruteforce', help='Enable the subbrute bruteforce module', nargs='?', default=False)
+    parser.add_argument('-p', '--ports', help='Scan the found subdomains against specified tcp ports')
+    parser.add_argument('-v', '--verbose', help='Enable Verbosity and display results in realtime', nargs='?',
+                        default=False)
+    parser.add_argument('-t', '--threads', help='Number of threads to use for subbrute bruteforce', type=int,
+                        default=30)
+    parser.add_argument('-e', '--engines', help='Specify a comma-separated list of search engines')
+    parser.add_argument('-o', '--output', help='Save just domain names to specified text file')
+    parser.add_argument('-a', '--analyze', default=False, help='Do reverse DNS analysis and output results', action="store_true")
+    parser.add_argument('--saverdns', help='Save reverse DNS analysis to specified file')
+    parser.add_argument('--inputfile', help='Read domains from specified file (perhaps from other tool) and use instead of searching engines. Use with -a to analyze domains')
+    parser.add_argument('--debug', default=False, help='Enable technical debug output', action="store_true")
+    parser.add_argument('-r', '--resolvers',  help='File with DNS servers to populate as resolvers, one per line')
+    parser.add_argument('-q', '--quiet', help='Show only result', default=False, action='store_true')
+    return parser.parse_args()
+
+
+def write_file(filename, subdomains):
+    # saving subdomains results to output file
+    print("%s[-] Saving results to file: %s%s%s%s" % (Y, W, R, filename, W))
+    with open(str(filename), 'wt') as f:
+        for subdomain in subdomains:
+            f.write(subdomain + "\r\n")
+
+
+def subdomain_sorting_key(hostname):
+    """Sorting key for subdomains
+    This sorting key orders subdomains from the top-level domain at the right
+    reading left, then moving '^' and 'www' to the top of their group. For
+    example, the following list is sorted correctly:
+    [
+        'example.com',
+        'www.example.com',
+        'a.example.com',
+        'www.a.example.com',
+        'b.a.example.com',
+        'b.example.com',
+        'example.net',
+        'www.example.net',
+        'a.example.net',
+    ]
+    """
+    parts = hostname.split('.')[::-1]
+    if parts[-1] == 'www':
+        return parts[:-1], 1
+    return parts, 0
+
+
+class enumratorBase(object):
+    def __init__(self, base_url, engine_name, domain, subdomains=None, silent=False, verbose=True):
+        subdomains = subdomains or []
+        self.domain = urlparse.urlparse(domain).netloc
+        self.session = requests.Session()
+        self.subdomains = []
+        self.timeout = 25
+        self.base_url = base_url
+        self.engine_name = engine_name
+        self.silent = silent
+        self.verbose = verbose
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Accept-Encoding': 'gzip',
+        }
+        self.print_banner()
+
+    def print_(self, text):
+        if not self.silent:
+            print(text)
+        return
+
+    def print_banner(self):
+        """ subclass can override this if they want a fancy banner :)"""
+        self.print_(G + "[-] Searching now in %s.." % (self.engine_name) + W)
+        return
+
+    def send_req(self, query, page_no=1):
+
+        url = self.base_url.format(query=query, page_no=page_no)
+        try:
+            resp = self.session.get(url, headers=self.headers, timeout=self.timeout)
+        except Exception:
+            resp = None
+        return self.get_response(resp)
+
+    def get_response(self, response):
+        if response is None:
+            return 0
+        return response.text if hasattr(response, "text") else response.content
+
+    def check_max_subdomains(self, count):
+        if self.MAX_DOMAINS == 0:
+            return False
+        return count >= self.MAX_DOMAINS
+
+    def check_max_pages(self, num):
+        if self.MAX_PAGES == 0:
+            return False
+        return num >= self.MAX_PAGES
+
+    # override
+    def extract_domains(self, resp):
+        """ chlid class should override this function """
+        return
+
+    # override
+    def check_response_errors(self, resp):
+        """ chlid class should override this function
+        The function should return True if there are no errors and False otherwise
+        """
+        return True
+
+    def should_sleep(self):
+        """Some enumrators require sleeping to avoid bot detections like Google enumerator"""
+        return
+
+    def generate_query(self):
+        """ chlid class should override this function """
+        return
+
+    def get_page(self, num):
+        """ chlid class that user different pagnation counter should override this function """
+        return num + 10
+
+    def enumerate(self, altquery=False):
+        flag = True
+        page_no = 0
+        prev_links = []
+        retries = 0
+
+        while flag:
+            query = self.generate_query()
+            count = query.count(self.domain)  # finding the number of subdomains found so far
+
+            # if they we reached the maximum number of subdomains in search query
+            # then we should go over the pages
+            if self.check_max_subdomains(count):
+                page_no = self.get_page(page_no)
+
+            if self.check_max_pages(page_no):  # maximum pages for Google to avoid getting blocked
+                return self.subdomains
+            resp = self.send_req(query, page_no)
+
+            # check if there is any error occured
+            if not self.check_response_errors(resp):
+                return self.subdomains
+            links = self.extract_domains(resp)
+
+            # if the previous page hyperlinks was the similar to the current one, then maybe we have reached the last page
+            if links == prev_links:
+                retries += 1
+                page_no = self.get_page(page_no)
+
+                # make another retry maybe it isn't the last page
+                if retries >= 3:
+                    return self.subdomains
+
+            prev_links = links
+            self.should_sleep()
+
+        return self.subdomains
+
